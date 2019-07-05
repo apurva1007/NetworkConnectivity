61107abbcf9ba0eecacb70ef7035322933bc71d8
Ruplag, Apurva <Apurva.Ruplag@cdk.com>
2019-07-05 14:52:38 +0530

    Optimization

--- find_largest_node_in_the_component.py
+++ find_largest_node_in_the_component.py
@@ -21,6 +21,8 @@ class FindLargestNodeInTheComponent:
     def union(self, p, q):
         root_p = self.find_root(p)
         root_q = self.find_root(q)
+        largest_p = self.largest[self.find_root(p)]
+        largest_q = self.largest[self.find_root(q)]
 
         if root_p == root_q:
             return
@@ -28,25 +30,31 @@ class FindLargestNodeInTheComponent:
         if self.size[root_p] < self.size[root_q]:
             self.parent[root_p] = root_q
             self.size[root_q] += self.size[root_p]
+
+            if largest_p > largest_q:
+                self.largest[root_q] = largest_p
         else:
             self.parent[root_q] = root_p
             self.size[root_p] += self.size[root_q]
 
+            if largest_q > largest_p:
+                self.largest[root_p] = largest_q
+
         self.count -= 1
 
-        largest_p = self.largest[p]
-        largest_q = self.largest[q]
-
-        if largest_p == largest_q:
-            return
-
-        for i in range(self.n):
-            if largest_p > largest_q:
-                if self.largest[i] == largest_q:
-                    self.largest[i] = largest_p
-            else:
-                if self.largest[i] == largest_p:
-                    self.largest[i] = largest_q
+        # largest_p = self.largest[p]
+        # largest_q = self.largest[q]
+        #
+        # if largest_p == largest_q:
+        #     return
+        #
+        # for i in range(self.n):
+        #     if largest_p > largest_q:
+        #         if self.largest[i] == largest_q:
+        #             self.largest[i] = largest_p
+        #     else:
+        #         if self.largest[i] == largest_p:
+        #             self.largest[i] = largest_q
 
     def connected(self, p, q):
         print("Connected Components:", self.count)
@@ -60,7 +68,7 @@ class FindLargestNodeInTheComponent:
         print("Parent Array: ", self.parent)
         print("Size Array: ", self.size)
         print("Largest Array: ", self.largest)
-        return self.largest[p]
+        return self.largest[self.find_root(p)]
 
 
 
